af222c1b6a0c430bf2dece2be853e39b0013ad75
Your Name <you@example.com>
2022-01-31 18:47:30 -0500

    added JavaScript to have button functionality

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,101 @@
-// HINT: You can delete this console.log after you no longer need it!
-console.log('JavaScript code has loaded!')
-// First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Caitlin Studer" // called in credit.textContent to change name
 
-// We'll use these variables to track the counts of each cookie type
-let gb = 0 // Gingerbread
-let cc = 0 // Chocolate Chip
-let sugar = 0 // Sugar Sprinkle
+let gb = 0 // gingerbread cookies
+let cc = 0 // chocolate chip cookies
+let sugar = 0 // sugar cookies
+let total = 0 // total quantity of cookies
 
-// selecting the element with an id of credit
-const credit = document.querySelector('#credit')
-// selecting the element with an id of add-gb
+const credit = document.querySelector('#credit') // selects the element needed to change name
+
+// functions for updating the quantity of each cookies and the total
+function updateGb(displayGb){
+    let gb = document.querySelector('#qty-gb')
+    gb.innerHTML = displayGb
+}
+
+function updateCc(displayCc){
+    let cc = document.querySelector('#qty-cc')
+    cc.innerHTML = displayCc
+}
+
+function updateSugar(displaySugar){
+    let sugar = document.querySelector('#qty-sugar')
+    sugar.innerHTML = displaySugar
+}
+
+function updateTotal(displayTotal){
+    let total = document.querySelector('#qty-total')
+    total.innerHTML = displayTotal
+}
+
+// selecting each button element 
 const gbPlusBtn = document.querySelector('#add-gb')
+const ccPlusBtn = document.querySelector('#add-cc')
+const sugarPlusBtn = document.querySelector('#add-sugar')
 
-// Code to update name display
+const gbMinusBtn = document.querySelector('#minus-gb')
+const ccMinusBtn = document.querySelector('#minus-cc')
+const sugarMinusBtn = document.querySelector('#minus-sugar')
+
+// actually changes the name
 credit.textContent = `Created by ${yourName}`
 
-// Event listener for clicks on the "+" button for Gingerbread cookies
-gbPlusBtn.addEventListener('click', function() {
-// HINT: You can delete this console.log after you no longer need it!
-console.log('Gingerbread + button was clicked!')
-
-// TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+// event listeners for gingerbread buttons
+gbPlusBtn.addEventListener('click', function(e){
+    gb++
+    total++
+    updateGb(`${gb}`)
+    updateTotal(`${total}`)
 })
 
-// TODO: Hook up event listeners for the rest of the buttons
+gbMinusBtn.addEventListener('click', function(e){
+    if (gb > 0){
+        gb--
+        total--
+    } else {
+        window.alert('You have no more gingerbread cookies in your cart')
+    }
+    updateGb(`${gb}`)
+    updateTotal(`${total}`)
+})
+
+// event listeners for chocolate chip buttons
+ccPlusBtn.addEventListener('click', function(e){
+    cc++
+    total++
+    updateCc(`${cc}`)
+    updateTotal(`${total}`)
+})
+
+ccMinusBtn.addEventListener('click', function(e){
+    if (cc > 0){
+        cc--
+        total--
+    } else {
+        window.alert('You have no more chocolate chip cookies in your cart')
+    }
+    updateCc(`${cc}`)
+    updateTotal(`${total}`)
+})
+
+// event listeners for sugar buttons
+sugarPlusBtn.addEventListener('click', function(e){
+    sugar++
+    total++
+    updateSugar(`${sugar}`)
+    updateTotal(`${total}`)
+})
+
+sugarMinusBtn.addEventListener('click', function(e){
+    if (sugar > 0){
+        sugar--
+        total--
+    } else {
+        window.alert('You have no more sugar cookies in your cart')
+    }
+    updateSugar(`${sugar}`)
+    updateTotal(`${total}`)
+})
+
+
+
